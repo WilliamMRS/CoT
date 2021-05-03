f0c443618fd6e96214e7c78d33adc03bea24a54b
jenss97 <74263010+jenss97@users.noreply.github.com>
2021-05-03 11:34:05 +0200

    Add files via upload

--- Værstasjon Trondheim.py
+++ Værstasjon Trondheim.py
@@ -6,42 +6,43 @@ Created on Wed Apr 14 12:57:36 2021
 """
 from metno_locationforecast import Place, Forecast
 
-def get_data_now():
+def Future_forecast():
     USER_AGENT = "metno_locationforecast/1.0 jenstho@stud.ntnu.no"
     Trondheim = Place("Trondheim", 63.42024, 10.40122)
     Trondheim_forecast = Forecast(Trondheim, USER_AGENT)
     Trondheim_forecast.update()
-   # First_interval = Trondheim_forecast.data.intervals[2]
     return(Trondheim_forecast)
-    
-#print( get_data_now())
+
+
+#print(Future_forecast())
 
 
 def getTemperature():
-    data = get_data_now().variables["air_temperature"]
+    data = Future_forecast().variables["air_temperature"]
     return data
 
 #print(getTemperature())
 
 def get_Cloud_area_fraction():
-    Cloud_data = get_data_now().variables["cloud_area_fraction"]
+    Cloud_data = Future_forecast().variables["cloud_area_fraction"]
     return Cloud_data
 #print(get_Cloud_area_fraction())
 
 def get_air_pressure():
-    pressure_data = get_data_now().variables["air_pressure_at_sea_level"]
+    pressure_data = Future_forecast().variables["air_pressure_at_sea_level"]
     return pressure_data
 #print(get_air_pressure())
 
-def future_forecast():
-    future_forecast = get_data_now()
-    First_interval = future_forecast.data.intervals[2]
+def Get_data_now():
+    get_data_now = Future_forecast()
+    First_interval = get_data_now.data.intervals[2]
     
     return First_interval
     
     
 
 print(future_forecast())
+
     
     
 
