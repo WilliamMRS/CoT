8e485fa952c0755289c658b7be7b6f5fbbf80719
Bendik Nygård <bendikn@gmail.com>
2021-05-15 01:14:16 +0200

    GJENNOMFØRT BOOKING MAFAKKA

--- Bendik/PROSJEKTET BENDIK/demo.py
+++ Bendik/PROSJEKTET BENDIK/demo.py
@@ -2,7 +2,7 @@
 import powerConsumers as pc
 import time
 import demo_functions as defunc
-
+import demoBooking as debok
 
 """ Alle tilgjengelige apparater: Flytt til README?
 
@@ -36,10 +36,12 @@ Merk: Alle curtains har samme CoT kode og vil justeres samtidig.
 
 """
 
-###___ Initialisering ___ ### 
+# ----------------
+    # Initialisering
+# ----------------
 
 
-timeInterval = 30 # Endre hyppighet for logging i sekunder
+timeInterval = 60 # Endre hyppighet for logging i sekunder
 
 pc.placeObjectsInRooms(pc.consumers, pc.rooms) # Initialiserer alle objektene. 
 oldTime = time.time()
@@ -47,14 +49,10 @@ oldTime = time.time()
 startTime = "20210513" # For strømpris og valuttakurs
 endTime = "20210514" # For strømpris og valuttakurs
 
-###___ Løkke___###
-
-while () :  # Kondisjon for å skru av og på?
-    if defunc.timePassed(oldTime, timeInterval) == True :
-        oldTime = time.time()
-        pc.updateConsumerStatus(pc.consumers) # Henter inn ny status 
-        pc.consumptionLogger(pc.rooms, timeInterval*20, startTime, endTime) # SKriver til CSV fil 
-
+bookingRooms = ["Bathroom", "Livingroom", "Kitchen"]
+idleTemp = 16
+useTemp = 22
+kitchenUses, bathroomUses, livingroomUses = 0, 0, 0 # For keeping track of num of Uses of a room during a day. 
 
 # Booking og bruksmønster
 
@@ -62,34 +60,130 @@ while () :  # Kondisjon for å skru av og på?
             # KUN BOOK MED 10 MINUTTERS INTERVAL
 # VIKTIG!!!
 
-# alle booker badet fra 07:30 til 09:00, 15 minutter hver.
-# folk booker kjøkkenet for en halvtime etter de har vært på badet hver. Det er da alltid bare 2 stk der samtidig. Fra 07:45 til 09:30
-# folk booker kjøkkenet for 15 minutter hver, to og to til lunsj. 12:00 til 13:00
-# 2 går på toalettet. 13:15 til 13:30
-# 4 går på toalettet fra 14:30 til 14:45
-# booker kjøkkenet 45 minutter hver, tre og tre. 16:30 til 17:15 og 17:15 til 18:00
-# 1,2,4 og 5 bruker stua fra 18:00 til 22:00
-# alle booker toalettet for 10 minutter før de legger seg, en og en. Mellom 22:00 og 23:00
 
-# Clear all bookings
+
+# Clear all bookings?
 
 # add all bookings planned for today
+
 # ----------------
     #Bookings
 # ----------------
 
-# def activateRoom(room) 
-#   If one or more is in room. activate
-#   return True
 
 
+# ----------------
+    # USAGE CONDITIONS
+# ----------------
 
-# Strømforbruk følger booking. 
+for index in range (0, 143) : # index = timeIntervall 1-144 
+    userLocation = { # For placing people in their own room as baseline
+        "livingroom" : {},
+        "kitchen" : {},
+        "bathroom" : {},
+        "bedroom_1" : {[1]},
+        "bedroom_2" : {[2]},
+        "bedroom_3" : {[3]},
+        "bedroom_4" : {[4]},
+        "bedroom_5" : {[5]},
+        "bedroom_6" : {[6]},
+}
+    pc.setConsumerStatus(0, pc.consumers) #Forces all objects to off state before checking who need to be turned on
+    for room in bookingRooms:
+        # Lower temperature inn all rooms before checking if there is people there
+            pc.consumers[str(room)+"Temp"].updateState(idleTemp)
+
+    for room in bookingRooms:
+        # Sjekker hvem som befinner seg hvor 
+        users = defunc.getRoomOccupants(index, bookingRooms) 
+        for i in users :
+            # plasserer personer i rommene de befinner seg i. 
+            print(i)
+            userLocation.update ({bookingRooms : users}) 
+
+
+    for key in userLocation : 
+    #______ Sjekker hvilket rom personer er plassert i og om eventuelle apparater skal skrus på ______#
+        if len(userLocation[key]) > 0 : # Sjekker om det befinner seg personer i rommet. 
+        
+            if key == "livingroom" : 
+                livingroomUses += 1
+                pc.consumers["livingroomTemp"].updateState(useTemp)
+                pc.consumers["TV"].updateState(1)
+
+            if key == "bathroom" :
+                bathroomUses += 1
+                pc.consumers["bathroomTemp"].updateState(23)
+                if index in range(35, 54) or index in range (108, 112) :
+                    # If morning or early evening. Assume people is going to the shower while visiting the bathroom 
+                    pc.consumers["shower"].updateState(1) 
+            
+            if key == "kitchen" :
+                kitchenUses += 1
+                pc.consumers["kitchenTemp"].updateState(useTemp)
+                pc.consumers["dishwasher"].updateState(1)
+                if kitchenUses % 4 == 0:
+                    pc.consumers["coffeMachine"].updateState(1)
+                if kitchenUses % 3 == 0:
+                    pc.consumers["stove"].updateState(1)
+
+            if key == "bedroom_1" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_1Temp"].updateState(idleTemp)
+                    pc.consumers("curtains_1").updateState(1)
+                else:
+                    pc.consumers["bedroom_1Temp"].updateState(useTemp)
+
+            if key == "bedroom_2" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_2Temp"].updateState(idleTemp)
+                    pc.consumers("curtains_2").updateState(1)
+                else:
+                    pc.consumers["bedroom_2Temp"].updateState(useTemp)
+            
+            if key == "bedroom_3" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_3Temp"].updateState(idleTemp)
+                    pc.consumers["curtains_3"].updateState(1)
+                else:
+                    pc.consumers["bedroom_3Temp"].updateState(useTemp)
+                    pc.consumers["curtains_1"].updateState(1)
+           
+            if key == "bedroom_4" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_4Temp"].updateState(idleTemp)
+                    pc.consumers["curtains_4"].updateState(1)
+                else:
+                    pc.consumers["bedroom_4Temp"].updateState(useTemp)
+       
+            if key == "bedroom_5" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_5Temp"].updateState(idleTemp)
+                    pc.consumers["curtains_5"].updateState(1)
+                else:
+                    pc.consumers["bedroom_5Temp"].updateState(useTemp)
+       
+            if key == "bedroom_6" :
+                if defunc.indexOfDay(index) :
+                    pc.consumers["bedroom_6Temp"].updateState(idleTemp)
+                    pc.consumers["curtains_6"].updateState(1)
+                else:
+                    pc.consumers["bedroom_6Temp"].updateState(useTemp)      
+    pc.consumers["fridge"].updateState(1) # Checks that Fridge always will be on
+
+# ----------------
+    # CHECKS COT STATUS & LOG
+# ----------------
+
+
+    if defunc.timePassed(oldTime, timeInterval) == True :
+        oldTime = time.time()
+        pc.updateConsumerStatus(pc.consumers) # Henter inn ny status 
+        pc.consumptionLogger(pc.rooms, timeInterval*20, startTime, endTime) # Skriver til CSV fil 
+
+
 
 # Dersom brukerID ikke er på ett annet rom antar vi at ID er på eget rom 
-# Skru av funksjoner på eget rom dersom vedkommende befinnner seg i ett annet rom. 
-
-
 
 # Ting skrur seg av å på etter hvor folk er. 
 
@@ -116,7 +210,7 @@ while () :  # Kondisjon for å skru av og på?
         # Ovn brukes 3. hver gang noen er på kjøkkenet. 
 
     # Soverom
-        # Temperatur på soverom settes til 18 grader om kvelden. 21 på dagtid dersom noen er der.
+        # Temperatur på soverom settes til 17 grader om kvelden. 21 på dagtid dersom noen er der.
         # For å spare signaler har alle gardiner samme kode. 
             # Sett disse til å heve senke ved legge- / våknetid? Evt. bare dropp de. 
     # Stue
