667c481c664a9c4ca7f8279541f54c595e3dde37
Bendik Nygård <bendikn@gmail.com>
2021-05-14 16:42:17 +0200

    QuickFix Weather

--- flaskServer/weather.py
+++ flaskServer/weather.py
@@ -5,21 +5,22 @@ def forecast():
     USER_AGENT = "metno-locationforecast/1.0 jenstho@stud.ntnu.no"
     Trondheim = Place("Trondheim", 63.42024, 10.40122)
     Trondheim_forecast = Forecast(Trondheim, USER_AGENT)
-    #Trondheim_forecast.update()
+    Trondheim_forecast.update()
     return(Trondheim_forecast)
-print(forecast())
+
 
 def liveForecast():
-    forecast = Future_forecast()
-    First_interval = forecast.data.intervals[2]
+    forecastData = forecast()
+    First_interval = forecastData.data.intervals[2]
     return First_interval
 
 def temperature():
-    data = str(Get_data_now().variables["air_temperature"]).split()[1].split("c")[0]
+    data = str(liveForecast().variables["air_temperature"]).split()[1].split("c")[0]
     return float(data)
-#print(getTemperature())
 
-def airPressure():
-    pressure_data = Future_forecast().variables["air_pressure_at_sea_level"]
+#print(temperature())
+
+""" def airPressure():
+    pressure_data = forecast().variables["air_pressure_at_sea_level"]
     return pressure_data
-#print(get_air_pressure())
+print(airPressure()) """
