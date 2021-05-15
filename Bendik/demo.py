4916cfcfcde0edb0bd6842ed09a245de4d6a4710
William M Schmidt <schmidtwilliammikal@gmail.com>
2021-05-15 02:04:51 +0200

    removed nested folder

--- Bendik/demo.py
+++ Bendik/demo.py
@@ -177,12 +177,15 @@ for index in range (0, 143) : # index = timeIntervall 1-144
         oldTime = time.time()
         pc.updateConsumerStatus(pc.consumers) # Henter inn ny status 
         pc.consumptionLogger(pc.rooms, timeInterval*20, startTime, endTime) # Skriver til CSV fil 
-        
-    with open('eggs.csv', 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+
+    with open('user_locations.csv', 'w', newline='') as csvfile:
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+        writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
 
 
 
